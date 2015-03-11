77fda69522b466d240e5bc81efb21a1456e675f7
ronatgithub <ronny.mombasa@gmail.com>
2015-03-11 23:00:31 +0100

    added google form extern link on katalog anfordern button click. removed modal window. added node.js googleapi library to package.json but not used.

--- packages/users/server/routes/users.js
+++ packages/users/server/routes/users.js
@@ -100,7 +100,8 @@ module.exports = function(MeanUser, app, auth, database, passport) {
       failureRedirect: '#!/login',
       scope: [
         'https://www.googleapis.com/auth/userinfo.profile',
-        'https://www.googleapis.com/auth/userinfo.email'
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.google.com/m8/feeds'
       ],accessType: 'offline', prompt: 'select_account'
     }), users.signin);
 
